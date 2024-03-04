92837fbfb8087b1ae84006eaf619260a26749baf
anton_baboshin <baboshin.ba@gmail.com>
2024-03-04 21:51:43 +0300

    feat: add about_info component

--- about/index.php
+++ about/index.php
@@ -3,7 +3,6 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("О нас");
 ?>
 
-
 <div class="center">
     <h1>Мы рады вас приветствовать</h1>
     <p class="short-line"></p>
@@ -12,65 +11,72 @@ $APPLICATION->SetTitle("О нас");
         стоимости.</p>
 </div>
 
-
-<div class="gray-block">
-    <div class="center">
-        <table class="three_col">
-            <tbody>
-                <tr>
-                    <td>
-                        <div class="inform_block">
-                            <div class="cell_wrapper text_cel">
-                                <section>Наша миссия</section>
-                            </div>
-                            <div class="cell_wrapper ico_cel">
-                                <section><img src="<?= SITE_TEMPLATE_PATH ?>/img/ico/ico_01.png" alt="" /></section>
-                            </div>
-                        </div>
-                        <p>Наша миссия заключается в повышении стоимости бизнеса наших клиентов благодаря
-                            профессиональному решению Ваших задач, связанных с привлечением капитала, анализом стоимости
-                            и эффективности Вашего бизнеса, оценкой активов. Мы нацелены на долгосрочное сотрудничество
-                            со своими клиентами, поэтому наш приоритет – это высокое качество работы. Мы с удовольствием
-                            поможем Вам решить Вашу задачу максимально эффективно и в срок.</p>
-                    </td>
-                    <td>
-                        <div class="inform_block">
-                            <div class="cell_wrapper text_cel">
-                                <section>Отраслевая специфика</section>
-                            </div>
-                            <div class="cell_wrapper ico_cel">
-                                <section><img src="<?= SITE_TEMPLATE_PATH ?>/img/ico/ico_02.png" alt="" /></section>
-                            </div>
-                        </div>
-                        <ul>
-                            <li>Розничная торговля и оказание услуг населению;</li>
-                            <li>Телекоммуникации, IT технологии и интернет проекты;</li>
-                            <li>Индустриальное производство;</li>
-                            <li>Строительство и девелоперские проекты;</li>
-                            <li>Энергетика и энергоэффективные технологии;</li>
-                            <li>Добыча и переработка полезных ископаемых.</li>
-                        </ul>
-                    </td>
-                    <td>
-                        <div class="inform_block">
-                            <div class="cell_wrapper text_cel">
-                                <section>Команда</section>
-                            </div>
-                            <div class="cell_wrapper ico_cel">
-                                <section><img src="<?= SITE_TEMPLATE_PATH ?>/img/ico/ico_03.png" alt="" /></section>
-                            </div>
-                        </div>
-                        <p>Наш капитал — наши сотрудники, которые вляются профессионалами в своей области, имеют
-                            начительный опыт работы в крупнейших международных и российских консалтинговых компаниях и
-                            банках. Перед началом работы мы обязательно направляем Вам краткое резюме ответственных
-                            исполнителей по Вашему проекту, чтобы Вы имели представление об имеющемся опыте сотрудника.
-                        </p>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-</div>
+<? $APPLICATION->IncludeComponent(
+    "bitrix:news.list",
+    "about_info_template",
+    array(
+        "ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
+        "ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
+        "AJAX_MODE" => "N",	// Включить режим AJAX
+        "AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
+        "AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
+        "AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
+        "AJAX_OPTION_STYLE" => "Y",	// Включить подгрузку стилей
+        "CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
+        "CACHE_GROUPS" => "Y",	// Учитывать права доступа
+        "CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+        "CACHE_TYPE" => "A",	// Тип кеширования
+        "CHECK_DATES" => "Y",	// Показывать только активные на данный момент элементы
+        "DETAIL_URL" => "",	// URL страницы детального просмотра (по умолчанию - из настроек инфоблока)
+        "DISPLAY_BOTTOM_PAGER" => "N",	// Выводить под списком
+        "DISPLAY_DATE" => "N",	// Выводить дату элемента
+        "DISPLAY_NAME" => "N",	// Выводить название элемента
+        "DISPLAY_PICTURE" => "Y",	// Выводить изображение для анонса
+        "DISPLAY_PREVIEW_TEXT" => "N",	// Выводить текст анонса
+        "DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+        "FIELD_CODE" => array(	// Поля
+            0 => "NAME",
+            1 => "PREVIEW_TEXT",
+            2 => "PREVIEW_PICTURE",
+            3 => "",
+        ),
+        "FILTER_NAME" => "",	// Фильтр
+        "HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
+        "IBLOCK_ID" => "5",	// Код информационного блока
+        "IBLOCK_TYPE" => "about_content",	// Тип информационного блока (используется только для проверки)
+        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",	// Включать инфоблок в цепочку навигации
+        "INCLUDE_SUBSECTIONS" => "N",	// Показывать элементы подразделов раздела
+        "MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
+        "NEWS_COUNT" => "3",	// Количество новостей на странице
+        "PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
+        "PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
+        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
+        "PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+        "PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
+        "PAGER_TEMPLATE" => "",	// Шаблон постраничной навигации
+        "PAGER_TITLE" => "Новости",	// Название категорий
+        "PARENT_SECTION" => "",	// ID раздела
+        "PARENT_SECTION_CODE" => "",	// Код раздела
+        "PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
+        "PROPERTY_CODE" => array(	// Свойства
+            0 => "",
+            1 => "",
+        ),
+        "SET_BROWSER_TITLE" => "N",	// Устанавливать заголовок окна браузера
+        "SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
+        "SET_META_DESCRIPTION" => "N",	// Устанавливать описание страницы
+        "SET_META_KEYWORDS" => "N",	// Устанавливать ключевые слова страницы
+        "SET_STATUS_404" => "N",	// Устанавливать статус 404
+        "SET_TITLE" => "N",	// Устанавливать заголовок страницы
+        "SHOW_404" => "N",	// Показ специальной страницы
+        "SORT_BY1" => "SORT",	// Поле для первой сортировки новостей
+        "SORT_BY2" => "",	// Поле для второй сортировки новостей
+        "SORT_ORDER1" => "ASC",	// Направление для первой сортировки новостей
+        "SORT_ORDER2" => "",	// Направление для второй сортировки новостей
+        "STRICT_SECTION_CHECK" => "N",	// Строгая проверка раздела для показа списка
+    ),
+    false
+); ?>
 
 <h2>Наши клиенты</h2>
 <div class="clients-slider_wrapper">
