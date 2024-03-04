ec5e18fb8fe642a52fdb255bb11c396c47d93824
anton_baboshin <baboshin.ba@gmail.com>
2024-03-04 21:58:19 +0300

    feat: add text page

--- text/index.php
+++ text/index.php
@@ -1,4 +1,26 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Текстовая");
-?>Страница "Текстовая"<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><blockquote>
+	<p style="text-align: justify;">
+	</p>
+</blockquote>
+<blockquote style="text-align: justify;">
+	<p style="text-align: justify;">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo elit at imperdiet dui. In fermentum posuere urna nec tincidunt. Posuere ac ut consequat semper. Nisi porta lorem mollis aliquam. Consectetur lorem donec massa sapien faucibus et. Neque aliquam vestibulum morbi blandit. Placerat vestibulum lectus mauris ultrices eros in cursus turpis. Tempor commodo ullamcorper a lacus vestibulum sed arcu. Consequat ac felis donec et odio pellentesque diam volutpat commodo. Massa enim nec dui nunc. Tortor vitae purus faucibus ornare. Eget duis at tellus at urna. Neque laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt. Viverra suspendisse potenti nullam ac tortor vitae. Pellentesque habitant morbi tristique senectus et netus et. Cursus vitae congue mauris rhoncus aenean.
+	</p>
+	Augue eget arcu dictum varius duis. Neque egestas congue quisque egestas. Tellus cras adipiscing enim eu turpis egestas. Consectetur adipiscing elit ut aliquam purus sit amet luctus. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae. Lectus mauris ultrices eros in cursus turpis massa. Nam libero justo laoreet sit amet cursus sit amet. Egestas integer eget aliquet nibh praesent tristique. Dui id ornare arcu odio ut sem nulla pharetra. Ut diam quam nulla porttitor massa. Interdum varius sit amet mattis vulputate enim. Egestas purus viverra accumsan in nisl nisi scelerisque eu.
+	<p>
+		Maecenas accumsan lacus vel facilisis volutpat est velit. Tincidunt dui ut ornare lectus. Lectus mauris ultrices eros in cursus turpis massa. Ante in nibh mauris cursus mattis molestie a. Dolor sit amet consectetur adipiscing elit ut aliquam purus sit. Faucibus interdum posuere lorem ipsum dolor sit. Mauris vitae ultricies leo integer malesuada nunc vel risus commodo. Cras adipiscing enim eu turpis egestas pretium aenean pharetra. Quam pellentesque nec nam aliquam sem et. Lectus urna duis convallis convallis tellus id interdum velit. Aliquet nec ullamcorper sit amet risus nullam. Gravida cum sociis natoque penatibus et magnis dis parturient montes. Blandit cursus risus at ultrices mi tempus imperdiet nulla. Id semper risus in hendrerit gravida rutrum quisque non. Diam maecenas sed enim ut sem.
+	</p>
+	<p>
+		Interdum consectetur libero id faucibus nisl tincidunt. Orci ac auctor augue mauris augue. Nulla at volutpat diam ut. Tempus iaculis urna id volutpat lacus laoreet non. Vel pretium lectus quam id leo in vitae turpis massa. Cras pulvinar mattis nunc sed blandit libero volutpat sed. Quisque id diam vel quam elementum pulvinar etiam non. Sed risus ultricies tristique nulla. Vel risus commodo viverra maecenas accumsan lacus. Urna nec tincidunt praesent semper. Turpis egestas sed tempus urna et pharetra pharetra massa massa. Sagittis id consectetur purus ut faucibus. Non blandit massa enim nec dui nunc mattis enim ut.
+	</p>
+	<p>
+		Neque egestas congue quisque egestas. Eu turpis egestas pretium aenean pharetra magna ac placerat. Turpis tincidunt id aliquet risus feugiat. Amet consectetur adipiscing elit ut. Sollicitudin nibh sit amet commodo nulla facilisi nullam. Volutpat ac tincidunt vitae semper. Ullamcorper sit amet risus nullam eget felis eget nunc. Nec ullamcorper sit amet risus. Gravida cum sociis natoque penatibus et magnis. Egestas fringilla phasellus faucibus scelerisque eleifend donec. Aliquam malesuada bibendum arcu vitae elementum curabitur. Id aliquet risus feugiat in ante metus dictum at tempor. Amet consectetur adipiscing elit duis tristique sollicitudin. Aliquet nec ullamcorper sit amet risus. Adipiscing enim eu turpis egestas pretium aenean pharetra. Diam in arcu cursus euismod quis. Ac feugiat sed lectus vestibulum mattis ullamcorper velit sed. Diam donec adipiscing tristique risus. Elit sed vulputate mi sit amet. Augue lacus viverra vitae congue.
+	</p>
+</blockquote>
+<p style="text-align: justify;">
+</p>
+<p>
+</p><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
